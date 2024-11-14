ae757da038d291d8a9520f78dd7baeeaa0f27d90
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-14 17:57:17 +0700

    Update main-multi.py

--- main-multi.py
+++ main-multi.py
@@ -49,8 +49,8 @@ PING_INTERVAL = 15  # Reduced ping interval for faster monitoring
 RETRIES = 3  # Limit retries to avoid delays
 
 DOMAIN_API = {
-    "SESSION": "http://18.136.143.169/api/auth/session",
-    "PING": "https://nw.nodepay.org/api/network/ping"
+    "SESSION": "https://api.nodepay.ai/api/auth/session",
+    "PING": "http://52.77.10.116/api/network/ping"
 }
 
 CONNECTION_STATES = {
